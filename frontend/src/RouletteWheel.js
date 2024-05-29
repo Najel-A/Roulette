3a6e4d1e088dc018f2a168dcbb08c7b7e3b748d7
Najel Alarcon <najelalarcon@gmail.com>
2024-05-28 22:37:40 -0700

    adding some stats

--- frontend/src/RouletteWheel.js
+++ frontend/src/RouletteWheel.js
@@ -8,42 +8,45 @@ import './Roulette.css'
 
 const data = [
   { option: '0', style: { backgroundColor: 'green', textColor: 'white' } },
-  { option: '32', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '15', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '19', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '4', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '21', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '2', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '25', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '17', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '34', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '6', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '27', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '13', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '36', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '11', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '30', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '8', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '23', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '10', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '5', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '24', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '16', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '33', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '1', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '20', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '14', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '31', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '9', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '22', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '18', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '29', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '7', style: { backgroundColor: 'red', textColor: 'white' } },
   { option: '28', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: '12', style: { backgroundColor: 'red', textColor: 'white' } },
-  { option: '35', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '9', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '26', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '30', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '11', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '7', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '20', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '32', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '17', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '5', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '22', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '34', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '15', style: { backgroundColor: 'black', textColor: 'white' } },
   { option: '3', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '24', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '36', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '13', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '1', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '00', style: { backgroundColor: 'green', textColor: 'white' } },
+  { option: '27', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '10', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '25', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '29', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '12', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '8', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '19', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '31', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '18', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '6', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '21', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '33', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '16', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '4', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '23', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '35', style: { backgroundColor: 'black', textColor: 'white' } },
+  { option: '14', style: { backgroundColor: 'red', textColor: 'white' } },
+  { option: '2', style: { backgroundColor: 'black', textColor: 'white' } }
 ];
+
 
 
 
@@ -52,6 +55,16 @@ const RouletteWheel = () => {
   const [isSpinning, setIsSpinning] = useState(false);
   const [prizeNumber, setPrizeNumber] = useState(0);
   const [array, setArray] = useState([]);
+
+  // Figure out stats here
+  const blackOptions = data.filter(item => item.style.backgroundColor === 'black').map(item => item.option);
+  const redOptions = data.filter(item => item.style.backgroundColor === 'red').map(item => item.option);
+  const greenOptions = data.filter(item => item.style.backgroundColor === 'green').map(item => item.option);
+
+  console.log("Black options:", blackOptions);
+  console.log("Red options:", redOptions);
+  console.log("Green options:", greenOptions);
+
 
   useEffect(() => {
     if (!isSpinning) {
