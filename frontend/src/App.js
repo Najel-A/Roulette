9f86d74f3adcfdccc990f9002dc3692ffcad21ce
Najel Alarcon <najelalarcon@gmail.com>
2024-06-19 20:38:45 -0700

    Adding files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import RouletteWheel from './RouletteWheel'; // Adjust the path accordingly
+import RouletteTable from './RouletteTable';
 
 const App = () => {
   return (
     <div>
       <RouletteWheel />
+
     </div>
   );
 };
